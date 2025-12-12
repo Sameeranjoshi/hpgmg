7184e97b8c1702bf002c4f10d11808b41866445a
Sameeranjoshi <joshisameeran17@gmail.com>
2025-12-12 16:44:04 -0700

    FP32 runs

---
+++ A100build/obj/finite-volume/source/mg.d
@@ -0,0 +1,26 @@
+obj/finite-volume/source/mg.o: \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/mg.c \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/timers.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/defines.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/level.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/operators.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/mg.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/cuda/common.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/cuda/extra.h
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/timers.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/defines.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/level.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/operators.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/mg.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/cuda/common.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/cuda/extra.h:
