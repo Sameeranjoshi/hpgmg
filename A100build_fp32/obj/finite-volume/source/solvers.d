3c884dbae050fd7cbafaef1e02d5a51911a17b98
Sameeranjoshi <joshisameeran17@gmail.com>
2025-12-15 22:39:00 -0700

    A100 runs, notch347. A100build_fp32 has results with jacobi at bottom, A100build_cg_fp32 has cg solver at bottom

---
+++ A100build_fp32/obj/finite-volume/source/solvers.d
@@ -0,0 +1,17 @@
+obj/finite-volume/source/solvers.o: \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers.c \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/timers.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/defines.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/level.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/operators.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers/bicgstab.c
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/timers.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/defines.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/level.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/operators.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers/bicgstab.c:
