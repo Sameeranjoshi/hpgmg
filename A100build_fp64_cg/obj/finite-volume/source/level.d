ac6c4149941ccafa9b40e00a507b43a060050c90
Sameeranjoshi <joshisameeran17@gmail.com>
2025-12-15 22:48:39 -0700

    A100 runs, notch347. fp32. No CG at bottom, only jacobi used

--- A100build_fp64_cg/obj/finite-volume/source/level.d
+++ A100build_fp64_cg/obj/finite-volume/source/level.d
@@ -1,10 +1,11 @@
-obj/finite-volume/source/solvers.o: \
- /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers.c \
+obj/finite-volume/source/level.o: \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/level.c \
  /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/timers.h \
  /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/defines.h \
  /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/level.h \
  /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/operators.h \
- /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers/bicgstab.c
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/cuda/common.h \
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/cuda/extra.h
 
 /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/timers.h:
 
@@ -14,4 +15,6 @@ obj/finite-volume/source/solvers.o: \
 
 /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/operators.h:
 
-/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers/bicgstab.c:
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/cuda/common.h:
+
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/cuda/extra.h:
--- A100build_fp64_cg/obj/finite-volume/source/solvers.d
+++ A100build_fp64_cg/obj/finite-volume/source/solvers.d
@@ -4,7 +4,7 @@ obj/finite-volume/source/solvers.o: \
  /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/defines.h \
  /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/level.h \
  /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/operators.h \
- /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers/bicgstab.c
+ /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers/cg.c
 
 /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/timers.h:
 
@@ -14,4 +14,4 @@ obj/finite-volume/source/solvers.o: \
 
 /uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/operators.h:
 
-/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers/bicgstab.c:
+/uufs/chpc.utah.edu/common/home/u1418973/hpgmg/hpgmg-cuda/finite-volume/source/solvers/cg.c:
